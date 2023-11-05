5a291c8b85fd81de4176a1f606a01af7ceffbc2f
Vitaliy <vvvital@i.ua>
2023-11-05 19:24:40 +0200

    update entity & repo

---
+++ src/main/java/com/vvvital/psychologistsmp/repository/PsychologistCardRepository.java
@@ -0,0 +1,7 @@
+package com.vvvital.psychologistsmp.repository;
+
+import com.vvvital.psychologistsmp.model.PsychologistCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PsychologistCardRepository extends JpaRepository<PsychologistCard,Long> {
+}
