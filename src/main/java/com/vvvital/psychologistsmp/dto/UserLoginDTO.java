97392a1f15f0030bc3bcb0ce241aaee3c11b72f7
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-11-05 20:46:14 +0200

    create userDTO

---
+++ src/main/java/com/vvvital/psychologistsmp/dto/UserLoginDTO.java
@@ -0,0 +1,17 @@
+package com.vvvital.psychologistsmp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDTO {
+
+    private String email;
+    private String password;
+
+}
