03265bee5804fa449f68fcaec251dd5e65f89d7d
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-11-06 23:02:59 +0200

    update DTO and add UserDTOMapper

---
+++ src/main/java/com/vvvital/psychologistsmp/dto/UserDTOMapper.java
@@ -0,0 +1,21 @@
+package com.vvvital.psychologistsmp.dto;
+
+import com.vvvital.psychologistsmp.model.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserDTOMapper {
+
+    UserResponseDTO userToUserResponseDTO(User user);
+
+    User responseDTOToUser(UserResponseDTO dto);
+
+    UserRequestDTO userToUserRequestDTO(User user);
+
+    User RequestDTOToUser(UserRequestDTO dto);
+
+    List<UserResponseDTO> usersToUserResponseDTO(List<User> users);
+
+}
