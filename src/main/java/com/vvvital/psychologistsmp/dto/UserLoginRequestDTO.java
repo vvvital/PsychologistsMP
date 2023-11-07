763b7deb336f6869645eb76682517e074a88406f
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-11-07 23:35:25 +0200

    update DTO and add UserController

--- src/main/java/com/vvvital/psychologistsmp/dto/UserLoginRequestDTO.java
+++ src/main/java/com/vvvital/psychologistsmp/dto/UserLoginRequestDTO.java
@@ -1,17 +1,29 @@
 package com.vvvital.psychologistsmp.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginRequestDTO {
 
     private String email;
     private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
--- src/main/java/com/vvvital/psychologistsmp/web/UserController.java
+++ src/main/java/com/vvvital/psychologistsmp/web/UserController.java
@@ -1,4 +1,61 @@
 package com.vvvital.psychologistsmp.web;
 
+import com.vvvital.psychologistsmp.dto.UserDTOMapper;
+import com.vvvital.psychologistsmp.dto.UserRequestDTO;
+import com.vvvital.psychologistsmp.dto.UserResponseDTO;
+import com.vvvital.psychologistsmp.model.User;
+import com.vvvital.psychologistsmp.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@Tag(name = "User")
 public class UserController {
+    private final UserService userService;
+    private final UserDTOMapper userDTOMapper;
+
+    public UserController(UserService userService, UserDTOMapper userDTOMapper) {
+        this.userService = userService;
+        this.userDTOMapper = userDTOMapper;
+    }
+
+    @PostMapping("/save")
+    @Operation(summary = "Save user")
+    public ResponseEntity<UserResponseDTO> save(@RequestBody UserRequestDTO userRequestDTO) {
+        User user = userDTOMapper.requestDTOToUser(userRequestDTO);
+        User savedUser = userService.save(user);
+        UserResponseDTO responseDTO = userDTOMapper.userToUserResponseDTO(savedUser);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @GetMapping("/email/{email}")
+    @Operation(summary = "Get user by email")
+    public ResponseEntity<UserResponseDTO> findByEmail(@PathVariable String email) {
+        User user = userService.findByEmail(email);
+        if (user != null) {
+            UserResponseDTO responseDTO = userDTOMapper.userToUserResponseDTO(user);
+            return ResponseEntity.ok(responseDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/all")
+    @Operation(summary = "Get all users")
+    public ResponseEntity<List<UserResponseDTO>> findAllUsersResponseDTO() {
+        List<UserResponseDTO> responseDTOs = userService.findAll();
+        return ResponseEntity.ok(responseDTOs);
+    }
+
+    @DeleteMapping("/delete")
+    @Operation(summary = "Delete user")
+    public ResponseEntity<String> delete(@RequestBody User user) {
+        userService.delete(user);
+        return ResponseEntity.ok("User deleted successfully");
+    }
 }
