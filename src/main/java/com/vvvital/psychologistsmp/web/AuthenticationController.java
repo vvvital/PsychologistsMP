f889f2862380ac86ad64a41f6a0d3aeb68520c94
Vitaliy <vvvital@i.ua>
2023-11-07 20:45:56 +0200

    add categories to psCard

--- src/main/java/com/vvvital/psychologistsmp/web/AuthenticationController.java
+++ src/main/java/com/vvvital/psychologistsmp/web/AuthenticationController.java
@@ -3,5 +3,5 @@ package com.vvvital.psychologistsmp.web;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class AuthenticationController {
 }
