9d0d17b2a03dbc2b1d1621e4a8f6af77d3c62702
Kovalenko Vitaliy <91799391+vvvital@users.noreply.github.com>
2023-11-08 15:32:18 +0200

    Update UserService.java

--- src/main/java/com/vvvital/psychologistsmp/service/UserService.java
+++ src/main/java/com/vvvital/psychologistsmp/service/UserService.java
@@ -1,40 +1,33 @@
 package com.vvvital.psychologistsmp.service;
 
-import com.vvvital.psychologistsmp.model.*;
+import com.vvvital.psychologistsmp.dto.UserDTOMapper;
+import com.vvvital.psychologistsmp.dto.UserResponseDTO;
+import com.vvvital.psychologistsmp.model.User;
 import com.vvvital.psychologistsmp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
-
     @Autowired
-    public UserService(UserRepository userRepository){
-        this.userRepository=userRepository;
-    }
+    private UserRepository userRepository;
 
     public User save(User user){
         return userRepository.save(user);
     }
 
-    public Optional<User> findByEmail(String email){
-        return userRepository.findByEmail(email);
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email).orElse(null);
     }
 
-    public List<User> findAll(Location location, Integer price, Integer rating, Integer experience, Set<Categories> categories) {
-        List<User> users=userRepository.findAll();
-        return users.stream().filter(user -> user.getLocation().equals(location)).collect(Collectors.toList());
+    public List<UserResponseDTO> findAll(){
+        return null;
     }
 
     public void delete(User user){
-        userRepository.delete(user);
+
     }
 }
