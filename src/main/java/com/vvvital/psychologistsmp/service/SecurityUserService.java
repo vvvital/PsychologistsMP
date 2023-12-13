9ce900d2b691b184d0ea39d4ff415808b16e4e95
Kovalenko Vitaliy <91799391+vvvital@users.noreply.github.com>
2023-12-13 11:16:40 +0200

    plugin security basic

---
+++ src/main/java/com/vvvital/psychologistsmp/service/SecurityUserService.java
@@ -0,0 +1,34 @@
+package com.vvvital.psychologistsmp.service;
+
+import com.vvvital.psychologistsmp.model.User;
+import com.vvvital.psychologistsmp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class SecurityUserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public SecurityUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userRepository.findByEmail(username).orElseThrow(()->
+                new UsernameNotFoundException(String.format("User %s not found",username)));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.name())).collect(Collectors.toList())
+        );
+    }
+}
