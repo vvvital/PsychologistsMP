c1bab9ec2e568965cf14a00d66d74c51db01fe03
Kovalenko Vitaliy <91799391+vvvital@users.noreply.github.com>
2023-12-21 20:21:16 +0200

    split service onto user & psychologist

--- src/main/java/com/vvvital/psychologistsmp/web/PsychologistController.java
+++ src/main/java/com/vvvital/psychologistsmp/web/PsychologistController.java
@@ -3,12 +3,11 @@ package com.vvvital.psychologistsmp.web;
 import com.vvvital.psychologistsmp.dto.PsychologistCardDTO;
 import com.vvvital.psychologistsmp.dto.PsychologistResponseDTO;
 import com.vvvital.psychologistsmp.dto.UserDTOMapper;
-import com.vvvital.psychologistsmp.dto.UserResponseDTO;
 import com.vvvital.psychologistsmp.model.Categories;
 import com.vvvital.psychologistsmp.model.Location;
 import com.vvvital.psychologistsmp.model.PsychologistCard;
 import com.vvvital.psychologistsmp.model.User;
-import com.vvvital.psychologistsmp.service.UserService;
+import com.vvvital.psychologistsmp.service.PsychologistService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -30,11 +29,11 @@ import java.util.stream.Collectors;
 @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH})
 public class PsychologistController {
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
-    private final UserService userService;
+    private final PsychologistService psychologistService;
     private final UserDTOMapper userDTOMapper;
 
-    public PsychologistController(UserService userService, UserDTOMapper userDTOMapper) {
-        this.userService = userService;
+    public PsychologistController(PsychologistService psychologistService, UserDTOMapper userDTOMapper) {
+        this.psychologistService = psychologistService;
         this.userDTOMapper = userDTOMapper;
     }
 
@@ -42,10 +41,10 @@ public class PsychologistController {
     @PostMapping("/save/{id}")
     @Operation(summary = "To become a psychologist",
             description = "User becomes a psychologist by id and add a psychologist card.")
-    public ResponseEntity<UserResponseDTO> becomePsychologist(@Parameter(description = "User's id") @PathVariable Long id,
+    public ResponseEntity<PsychologistResponseDTO> becomePsychologist(@Parameter(description = "User's id") @PathVariable Long id,
                                                               @RequestBody PsychologistCardDTO card) {
-        User becomePsychologist = userService.becomePsychologist(id, card);
-        UserResponseDTO responseDTO = userDTOMapper.userToUserResponseDTO(becomePsychologist);
+        User becomePsychologist = psychologistService.becomePsychologist(id, card);
+        PsychologistResponseDTO responseDTO = PsychologistResponseDTO.toDTO(becomePsychologist);
         return ResponseEntity.ok(responseDTO);
     }
 
@@ -67,7 +66,7 @@ public class PsychologistController {
         if (categories != null) {
             categoriesSet = Arrays.stream(categories).map(Categories::valueOf).collect(Collectors.toSet());
         }
-        return ResponseEntity.ok(userService.findAllPsych(Location.valueOf(location), strToInt(priceMin), strToInt(priceMax)
+        return ResponseEntity.ok(psychologistService.findAllPsych(Location.valueOf(location), strToInt(priceMin), strToInt(priceMax)
                 , strToInt(ratingMin), strToInt(ratingMax), strToInt(experienceMin), strToInt(experienceMax), categoriesSet, order));
     }
 
@@ -75,7 +74,7 @@ public class PsychologistController {
     @Operation(summary = "Get psychologist by Id")
     public ResponseEntity<?> get(@PathVariable Long id) {
         try {
-            User psychologist = userService.getById(id);
+            User psychologist = psychologistService.getById(id);
             return ResponseEntity.ok(PsychologistResponseDTO.toDTO(psychologist));
         }catch (UsernameNotFoundException e){
             return ResponseEntity.badRequest().body(e.getMessage());
@@ -89,7 +88,7 @@ public class PsychologistController {
     )
     public ResponseEntity<?> updatePsychologistCard(@Parameter(description = "Psychologist's card id") @PathVariable Long id, @RequestBody PsychologistCardDTO cardDTO, Principal principal) {
         try {
-            PsychologistCard updatePsychologistCard = userService.updatePsychologistCard(id, cardDTO, principal);
+            PsychologistCard updatePsychologistCard = psychologistService.updatePsychologistCard(id, cardDTO, principal);
             PsychologistCardDTO responseDTO = PsychologistCardDTO.toDTO(updatePsychologistCard);
             return ResponseEntity.ok(responseDTO);
         } catch (Exception e) {
@@ -102,7 +101,7 @@ public class PsychologistController {
             description = "Update SOME variables a psychologist card by specifying it's id. The response is psychologist card with price, rating, experience, description, photoLink and categories.")
     public ResponseEntity<?> patchPsychologistCard(@Parameter(description = "Psychologist's card id") @PathVariable Long id, @RequestBody PsychologistCardDTO cardDTO, Principal principal) {
         try {
-            PsychologistCard patchedPsychologistCard = userService.patchPsychologistCard(id, cardDTO, principal);
+            PsychologistCard patchedPsychologistCard = psychologistService.patchPsychologistCard(id, cardDTO, principal);
             PsychologistCardDTO responseDTO = PsychologistCardDTO.toDTO(patchedPsychologistCard);
             return ResponseEntity.ok(responseDTO);
         } catch (Exception e) {
