119e73a709bb7c16fcc06c74e7db4009c22fc8c3
Kovalenko Vitaliy <91799391+vvvital@users.noreply.github.com>
2023-12-24 21:26:27 +0200

    hid environment variables

--- src/main/java/com/vvvital/psychologistsmp/web/AuthenticationController.java
+++ src/main/java/com/vvvital/psychologistsmp/web/AuthenticationController.java
@@ -1,7 +1,13 @@
 package com.vvvital.psychologistsmp.web;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthenticationController {
+
+    @PostMapping("/logout")
+    public void logout(){}
 }
