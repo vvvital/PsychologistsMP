0d06ec2b06ecc916f7bf148cb02a46f1d1a7b4b9
Kovalenko Vitaliy <91799391+vvvital@users.noreply.github.com>
2024-01-16 21:08:32 +0200

    cors in security config

--- src/main/java/com/vvvital/psychologistsmp/config/SecurityConfig.java
+++ src/main/java/com/vvvital/psychologistsmp/config/SecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -35,7 +38,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    corsConfiguration.setAllowCredentials(true);
+                    return corsConfiguration;
+                }))
                 //.httpBasic(basic -> basic.init(http))
                 .logout(logout->logout.logoutUrl("/auth/logout"))
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
